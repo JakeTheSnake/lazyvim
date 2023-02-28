b4e526790e5117d000977cb9a69ed406dc3c6dbe
Jakob Ström <jakob.strom@nordicmedtest.se>
2023-02-28 14:18:27 +0100

    telescope binds

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        mappings = {
+          i = {
+            ["<C-j>"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+            ["<C-p>"] = function(...)
+              return require("telescope.actions.layout").toggle_preview(...)
+            end,
+          },
+          n = {
+            ["j"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["k"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+            ["gg"] = function(...)
+              return require("telescope.actions").move_to_top(...)
+            end,
+            ["G"] = function(...)
+              return require("telescope.actions").move_to_bottom(...)
+            end,
+            ["<C-p>"] = function(...)
+              return require("telescope.actions.layout").toggle_preview(...)
+            end,
+          },
+        },
+      },
+    },
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+    -- can't get the config to work, so it's just disabled for now
+    opts = function()
+      return {
+        mappings = {
+          ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+          ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+          ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+          [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+          ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+          ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+          -- ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+          -- ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+          ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+        },
+      }
+    end,
+  },
+}
