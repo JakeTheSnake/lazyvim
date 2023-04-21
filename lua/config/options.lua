0ea69924edd41c2aa4b0595381fcc47f7c94d700
Jakob Ström <jacobo.strm@gmail.com>
2023-04-21 10:19:22 +0200

    do not add comment on o

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable auto comment when insert new line after comment
+local opt = vim.opt
+
+vim.api.nvim_create_autocmd("bufEnter", {
+  group = vim.api.nvim_create_augroup("FormatOptions", {}),
+  pattern = "*",
+  callback = function()
+    opt.formatoptions:remove({ "r", "o" })
+  end,
+})
