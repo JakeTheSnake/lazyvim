3bab1725d63d2591837351450694096c31241741
Jakob Ström <jacobo.strm@gmail.com>
2025-09-23 22:22:51 +0200

    remove mason workaround

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "yaml",
+  callback = function()
+    vim.lsp.start({
+      cmd = { "openapi-language-server" },
+      filetypes = { "yaml" },
+      root_dir = vim.fn.getcwd(),
+    })
+  end,
+})
