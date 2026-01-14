986b7f72bfff483272d2776472657868511ebbfb
Jakob Ström <jakob.strom@inera.se>
2026-01-14 16:29:02 +0100

    fix helm deprecation

--- lua/plugins/helm.lua
+++ lua/plugins/helm.lua
@@ -44,13 +44,13 @@ return {
 
       setup = {
         yamlls = function()
-          LazyVim.lsp.on_attach(function(client, buffer)
+          require("snacks.util.lsp").on({ name = "yamlls" }, function(client, buffer)
             if vim.bo[buffer].filetype == "helm" then
               vim.schedule(function()
                 vim.cmd("LspStop ++force yamlls")
               end)
             end
-          end, "yamlls")
+          end)
         end,
       },
     },
