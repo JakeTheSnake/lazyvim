13295437d2a28a0b7bec3e39a482764321894b55
Jakob Ström <jakob.strom@inera.se>
2026-01-14 16:29:12 +0100

    remove yaml

--- lua/plugins/yaml.lua
+++ lua/plugins/yaml.lua
@@ -1,82 +1,83 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "yaml" })
-      end
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "b0o/SchemaStore.nvim",
-      version = false, -- last release is way too old
-    },
-    opts = {
-      setup = {
-        yamlls = function()
-          require("lazyvim.util").lsp.on_attach(function(client, bufnr)
-            if client.name == "yamlls" and vim.bo.filetype == "helm" then
-              vim.lsp.stop_client(bufnr, client.id)
-            end
-          end)
-        end,
-      },
-      -- make sure mason installs the server
-      servers = {
-        yamlls = {
-          -- Have to add this for yamlls to understand that we support line folding
-          capabilities = {
-            textDocument = {
-              foldingRange = {
-                dynamicRegistration = false,
-                lineFoldingOnly = true,
-              },
-            },
-          },
-          -- lazy-load schemastore when needed
-          on_new_config = function(new_config)
-            new_config.settings.yaml.schemas = new_config.settings.yaml.schemas or {}
-            vim.list_extend(new_config.settings.yaml.schemas, require("schemastore").yaml.schemas())
-          end,
-          settings = {
-            redhat = { telemetry = { enabled = false } },
-            yaml = {
-              keyOrdering = false,
-              format = {
-                enable = true,
-              },
-              validate = true,
-              hover = true,
-              completion = true,
-              schemaStore = {
-                -- Must disable built-in schemaStore support to use
-                -- schemas from SchemaStore.nvim plugin
-                enable = false,
-                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-                url = "",
-              },
-              -- schemas = {
-              --   ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-              --   ["https://json.schemastore.org/cert-manager"] = "*.cert.yaml",
-              --   ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-              --   ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-              --   ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-              --   ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-              --   ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-              --   ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-              --   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-              --   ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-              --
-              --   -- kubernetes = "*.yaml",
-              -- },
-            },
-          },
-        },
-      },
-    },
-  },
+  --   {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     opts = function(_, opts)
+  --       if type(opts.ensure_installed) == "table" then
+  --         vim.list_extend(opts.ensure_installed, { "yaml" })
+  --       end
+  --     end,
+  --   },
+  --   {
+  --     "neovim/nvim-lspconfig",
+  --     dependencies = {
+  --       "b0o/SchemaStore.nvim",
+  --       version = false, -- last release is way too old
+  --     },
+  --     opts = {
+  --       setup = {
+  --         yamlls = function()
+  --           Snacks.util.lsp.on(function(_, client)
+  --             if client.name == "yamlls" and vim.bo.filetype == "helm" then
+  --               vim.lsp.stop_client(client.id)
+  --             end
+  --           end)
+  --         end,
+  --       },
+  --       -- make sure mason installs the server
+  --       servers = {
+  --         yamlls = {
+  --           -- Have to add this for yamlls to understand that we support line folding
+  --           capabilities = {
+  --             textDocument = {
+  --               foldingRange = {
+  --                 dynamicRegistration = false,
+  --                 lineFoldingOnly = true,
+  --               },
+  --             },
+  --           },
+  --           -- lazy-load schemastore when needed
+  --           on_new_config = function(new_config)
+  --             new_config.settings.yaml.schemas = new_config.settings.yaml.schemas or {}
+  --             vim.list_extend(new_config.settings.yaml.schemas, require("schemastore").yaml.schemas())
+  --           end,
+  --           settings = {
+  --             redhat = { telemetry = { enabled = false } },
+  --             yaml = {
+  --               keyOrdering = false,
+  --               format = {
+  --                 enable = true,
+  --               },
+  --               validate = true,
+  --               hover = true,
+  --               completion = true,
+  --               schemaStore = {
+  --                 -- Must disable built-in schemaStore support to use
+  --                 -- schemas from SchemaStore.nvim plugin
+  --                 enable = false,
+  --                 -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+  --                 url = "",
+  --               },
+  --               schemas = require("schemastore").yaml.schemas(),
+  --               -- schemas = {
+  --               --   ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+  --               --   ["https://json.schemastore.org/cert-manager"] = "*.cert.yaml",
+  --               --   ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+  --               --   ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+  --               --   ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+  --               --   ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+  --               --   ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+  --               --   ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+  --               --   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+  --               --   ["https://raw.githubusercontent.com/argoproj/argo-workflows/main/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+  --               --
+  --               --   kubernetes = "*.yaml",
+  --               -- },
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
   {
     "someone-stole-my-name/yaml-companion.nvim",
     dependencies = {
@@ -111,8 +112,8 @@ return {
         -- Additional schemas available in Telescope picker
         schemas = {
           {
-            name = "Kubernetes 1.33.4",
-            uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.33.4-standalone-strict/all.json",
+            name = "Argo Workflows",
+            uri = "https://raw.githubusercontent.com/argoproj/argo-workflows/main/api/jsonschema/schema.json",
           },
         },
       })
